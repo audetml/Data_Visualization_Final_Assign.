7e0452101065cfeb45d7787baf3ccc3554f918c6
audetml <132603737+audetml@users.noreply.github.com>
2023-08-09 09:22:53 -0400

    Update DV0101EN-Final_Assign_Part_2_Questions (3).py
    Final

--- DV0101EN-Final_Assign_Part_2_Questions (3).py
+++ DV0101EN-Final_Assign_Part_2_Questions (3).py
@@ -115,16 +115,17 @@ def update_output_container(selected_statistics, input_year):
 
 
         return [
-            html.Div(className='chart-item', children=[html.Div(children=R_chart1),html.Div(children=R_chart2)], style={"display":"flex"}),
-            html.Div(className='chart-item', children=[html.Div(children=R_chart3),html.Div(children=R_chart4)], style={"display":"flex"})]
-
-
+            html.Div(className='chart-grid', children=[html.Div(children=R_chart1),html.Div(children=R_chart2)],style={'display': 'flex'}),
+            html.Div(className='chart-item', children=[html.Div(children=R_chart3),html.Div(children=R_chart4)],style={'display': 'flex'})
+            ]
 
 # TASK 2.6: Create and display graphs for Yearly Report Statistics
  # Yearly Statistic Report Plots                             
-elif (input_year and selected_statistics=='Yearly Statistics'):
-yearly_data=data[data['Year']==input_year]
-                              
+    elif (input_year and selected_statistics== year_list and 'Yearly Statistics') :
+        yearly_data = data[data['Year'] == input_year]
+
+
+
                               
 #plot 1 Yearly Automobile sales using line chart for the whole period.
         yas= data.groupby('Year')['Automobile_Sales'].mean().reset_index()
